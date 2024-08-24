c5167d87d673413468be7ba4c6a9ca14fa3d16fc
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-24 22:25:17 +0100

    auto-commit

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/product', async (req, res, next) => {
+router.get('/products', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
