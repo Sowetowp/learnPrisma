67a834ec3bb1d2100e0177c71b0b44a6e78f68e5
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-24 22:26:55 +0100

    auto-commit

--- routes/api.route.js
+++ routes/api.route.js
@@ -16,7 +16,7 @@ router.delete('/products/:id', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
-router.get('/products', async (req, res, next) => {
+router.patch('/products', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
