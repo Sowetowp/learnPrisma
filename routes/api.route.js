e6650f90fda0323fa3a73b6b44e3bc1efb8ab8bd
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-24 23:09:20 +0100

    auto-commit

--- routes/api.route.js
+++ routes/api.route.js
@@ -4,7 +4,9 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 router.get('/products', async (req, res, next) => {
 try {
-  const products = await prisma.product.findMany({})
+  const products = await prisma.product.findMany({
+    include:{category}
+  })
   res.json(products)
 } catch (error) {
   next(error)
